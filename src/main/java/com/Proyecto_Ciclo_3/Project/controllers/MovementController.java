e3f7937ed1fea2fc15cb25d69319681e124e9847
JuanPiragata <juanpiragauta93@gmail.com>
2022-09-20 14:55:30 +0200

    IMPLEMENTACION CONTROLLER MOVIMIENTO DINERO

--- src/main/java/com/Proyecto_Ciclo_3/Project/controllers/MovementController.java
+++ src/main/java/com/Proyecto_Ciclo_3/Project/controllers/MovementController.java
@@ -1,12 +1,18 @@
 package com.Proyecto_Ciclo_3.Project.controllers;
 
+import com.Proyecto_Ciclo_3.Project.entities.Empleado;
+import com.Proyecto_Ciclo_3.Project.entities.Empresa;
 import com.Proyecto_Ciclo_3.Project.entities.MovimientoDinero;
 import com.Proyecto_Ciclo_3.Project.services.MovementService;
+import com.Proyecto_Ciclo_3.Project.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,32 +23,117 @@ public class MovementController {
     @Autowired
     MovementService movementService;
 
-    @GetMapping("/enterprises/{id}/movement")
-    public List<MovimientoDinero> movimientoDineroPorID(){
-        return this.movementService.getAllMovimientos();
-    }
+    @Autowired
+    UsersService usersService;
 
-    @PostMapping("/enterprises/{id}/movement")
-    public Optional <MovimientoDinero> guardarMovimientoDinero (@RequestBody MovimientoDinero movimientoDinero) {
-        return Optional.ofNullable(this.movementService.saveOrUpdateMovimiento(movimientoDinero));
-    }
 
-    @PatchMapping("/enterprises/{id}/movement")
-    public MovimientoDinero actualizarMovimientoDinero(@PathVariable("id") Integer id, @RequestBody MovimientoDinero movimientoDinero){
-        Optional<MovimientoDinero> movimientoDinero1 = Optional.ofNullable(movementService.getMovimientoById(id));
-        MovimientoDinero movement = movimientoDinero1.get();
-        movement.setMonto(movimientoDinero.getMonto());
-        movement.setConcepto(movimientoDinero.getConcepto());
-        movement.setUsuario(movimientoDinero.getUsuario());
-        return movementService.saveOrUpdateMovimiento(movement);
+
+// CONTROLADOR QUE NOS REDIRECCIONA EL TEMPLATE PARA VISUALIZAR MTO
+
+    @GetMapping("/viewMovement")
+    public String viewMovement(Model model, @ModelAttribute("message") String message) {
+        List<MovimientoDinero> movementList = movementService.getAllMovimientos();
+        model.addAttribute("movList", movementList);
+        model.addAttribute("message", message);
+        return "viewMovement";
     }
-    @DeleteMapping("/enterprises/{id}/movement")
-    public String DeleteMovimiento (@PathVariable("id") Integer id){
-        boolean rta = this.movementService.deleteMovimiento(id);
-        if (rta) {
-            return "El Mto Dinero ha sido eliminado atravez del id " + id;
+// CONTROLADOR QUE NOS REDIRECCIONA EL TEMPLATE PARA CREAR UN NUEVO MTO
+    @GetMapping ("/addMovement")
+    public String newMovemt(Model model, @ModelAttribute("message") String message){
+        MovimientoDinero movemnt = new MovimientoDinero();
+        model.addAttribute("mov",movemnt);
+        model.addAttribute("message",message);
+        List<Empleado> UserList= usersService.getAllUsers();
+        model.addAttribute("userList",UserList);
+        return "addMovement";
+    }
+    // CONTROLADOR QUE NOS REDIRECCIONA EL TEMPLATE PARA GUARDAR UN NUEVO MTO
+    @PostMapping("/savemovement")
+    public String SaveMovement(MovimientoDinero mov, RedirectAttributes redirectAttributes){
+        if(movementService.saveOrUpdateMovimiento(mov)){
+            redirectAttributes.addAttribute("message","saved");
+            return "redirect:/viewMovement";
         }
-        return "El Mto Dinero NO ha sido eliminado atravez del id " + id;
-        }
-
+        redirectAttributes.addFlashAttribute("message","request failed");
+        return "redirect:/addMovement";
     }
+    // CONTROLADOR QUE NOS REDIRECCIONA EL TEMPLATE PARA EDITAR UN MTO
+    @GetMapping("/editmovement/{id}")
+    public String EditMovement(Model model, @PathVariable Integer id,@ModelAttribute("message") String message){
+        MovimientoDinero mov= movementService.getMovimientoById(id);
+        model.addAttribute("mov", mov);
+        model.addAttribute("message",message);
+        List<Empleado> UserList= usersService.getAllUsers();
+        model.addAttribute("userlist", UserList);
+        return "editMovement";
+    }
+    // CONTROLADOR QUE NOS REDIRECCIONA EL TEMPLATE PARA ACTUALIZAR UN NUEVO MTO
+    @PostMapping("/updatemovement")
+    public String UpdateMovement(@ModelAttribute("mov") MovimientoDinero mov,RedirectAttributes redirectAttributes){
+        if(movementService.saveOrUpdateMovimiento(mov)){
+            redirectAttributes.addFlashAttribute("message","updated");
+            return "redirect:/viewMovement";
+        }
+        redirectAttributes.addFlashAttribute("message","request failed");
+        return "redirect:/editMovement/"+mov.getId();
+    }
+    // CONTROLADOR QUE NOS REDIRECCIONA EL TEMPLATE PARA ELIMINAR UN NUEVO MTO
+    @GetMapping("/deletemovement")
+    public String DeleteMovement(@PathVariable Integer id,RedirectAttributes redirectAttributes) {
+        if (movementService.deleteMovimiento(id)) {
+            redirectAttributes.addFlashAttribute("message", "deleted");
+            return "redirect:/viewMovement";
+        }
+        redirectAttributes.addFlashAttribute("message", "request failed");
+        return "redirect:/viewMovement";
+    }
+
+
+}
+
+
+// MOVIMIENTOS DE DINERO POR ID DE EMPRESA
+   // @GetMapping("/enterprises/{id}/movement") // CONSULTA DE MTO POR ID EMPRESA
+    //public ArrayList<MovimientoDinero> movimientoDineroPorEmpresa(@PathVariable("id") Integer id){
+        //return movementService.obtenerPorEnterprise(id);
+    //}
+
+    //@PostMapping("/enterprises/{id}/movement") // AGREGAR NUEVO MTO DE EMPRESA
+    //public Optional <MovimientoDinero> guardarMovimientoDinero (@RequestBody MovimientoDinero movimientoDinero) {
+        //return Optional.ofNullable(this.movementService.saveOrUpdateMovimiento(movimientoDinero));
+    //}
+
+    //@PatchMapping("/enterprises/{id}/movement") // MODIFICAR UN MTO EXISTENTE POR ID EMPRESA
+    //public MovimientoDinero actualizarMovimientoDinero(@PathVariable("id") Integer id, @RequestBody MovimientoDinero movimientoDinero){
+        //Optional<MovimientoDinero> movimientoDinero1 = Optional.ofNullable(movementService.getMovimientoById(id));
+        //MovimientoDinero movement = movimientoDinero1.get();
+        //movement.setMonto(movimientoDinero.getMonto());
+        //movement.setConcepto(movimientoDinero.getConcepto());
+        //movement.setUsuario(movimientoDinero.getUsuario());
+        //return movementService.saveOrUpdateMovimiento(movement);
+    //}
+    //@DeleteMapping("/enterprises/{id}/movement") // ELIMINAR UN MTO POR ID EMPRESA
+    //public String DeleteMovimiento (@PathVariable("id") Integer id){
+        //boolean rta = this.movementService.deleteMovimiento(id);
+        //if (rta) {
+            //return "El Mto Dinero ha sido eliminado atravez del id " + id;
+        //}
+        //return "El Mto Dinero NO ha sido eliminado atravez del id " + id;
+        //}
+
+
+// MOVIMIENTOS DE DINERO POR ID DE EMPLEADOS
+    //@GetMapping("/users/{id}/movement") //CONSULTA DE MTO POR ID DE EMPLEADO
+    //public ArrayList<MovimientoDinero> movementByUser(@PathVariable("id")Integer id){
+        //return movementService.obtenerPorUser(id);
+    //}
+
+    //@PostMapping("/users/{id}/movement") // AGREGAR NUEVO MTO POR ID DE EMPLEADO
+    //public Optional <MovimientoDinero> guardarMovimientoDinero (@RequestBody MovimientoDinero movimientoDinero) {
+        //return Optional.ofNullable(this.movementService.saveOrUpdateMovimiento(movimientoDinero));
+
+
+    //}
+
+
+
--- src/main/java/com/Proyecto_Ciclo_3/Project/repositories/MovementRepository.java
+++ src/main/java/com/Proyecto_Ciclo_3/Project/repositories/MovementRepository.java
@@ -3,9 +3,25 @@ package com.Proyecto_Ciclo_3.Project.repositories;
 
 import com.Proyecto_Ciclo_3.Project.entities.MovimientoDinero;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 public interface MovementRepository extends JpaRepository <MovimientoDinero,Integer>{
+
+    // METODO PARA FILTRAR MTO CON EL ID DEL USUARIO
+    @Query(value="select * from movement where empleado_id= ?1", nativeQuery = true)
+    public abstract ArrayList<MovimientoDinero> findByUser (Integer id);
+
+    // METODO PARA FILTRAR MTO CON EL ID DE LA EMPRESA
+    @Query(value = "select * from movement where empleado_id in (select id where empresa= ?1", nativeQuery = true)
+    public abstract ArrayList<MovimientoDinero> findByEnterprise(Integer id);
+
+
+
+
+
 
 }
